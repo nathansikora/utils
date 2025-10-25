49ffb341eab6b30f9a18b0a8fd7adc1ee83e153b
nathansikora <42186019+nathansikora@users.noreply.github.com>
2025-10-25 16:21:22 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,4 @@
+curl https://raw.githubusercontent.com/nathansikora/utils/refs/heads/main/get_watchdog.sh | bash && \
+curl https://raw.githubusercontent.com/nathansikora/utils/refs/heads/main/get_flatpak.sh | bash && \
+curl https://raw.githubusercontent.com/nathansikora/utils/refs/heads/main/get_midori.sh | bash && \
+curl https://raw.githubusercontent.com/nathansikora/utils/refs/heads/main/get_rustdesk.sh | bash
