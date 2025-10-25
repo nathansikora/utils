9b01b8b512e770a6e62d9a9c5b22dcc7ea7c8930
nathansikora <42186019+nathansikora@users.noreply.github.com>
2025-10-25 17:56:23 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo && \
-sudo apt update && \
-sudo apt upgrade --fix-broken && \
+sudo apt update -y && \
+sudo apt upgrade -y --fix-broken && \
+sudo apt autoremove -y && \ 
+sudo apt autoclean -y && \
 curl https://raw.githubusercontent.com/nathansikora/utils/refs/heads/main/get_watchdog.sh | bash && \
 curl https://raw.githubusercontent.com/nathansikora/utils/refs/heads/main/get_flatpak.sh | bash && \
 curl https://raw.githubusercontent.com/nathansikora/utils/refs/heads/main/get_midori.sh | bash && \
