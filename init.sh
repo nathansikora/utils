142241ecf8432be861c920c576ebe602f8d6bb15
nathansikora <42186019+nathansikora@users.noreply.github.com>
2025-10-25 16:21:59 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+sudo apt update && \
+sudo apt upgrade --fix-broken && \
 curl https://raw.githubusercontent.com/nathansikora/utils/refs/heads/main/get_watchdog.sh | bash && \
 curl https://raw.githubusercontent.com/nathansikora/utils/refs/heads/main/get_flatpak.sh | bash && \
 curl https://raw.githubusercontent.com/nathansikora/utils/refs/heads/main/get_midori.sh | bash && \
